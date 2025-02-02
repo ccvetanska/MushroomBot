1a7c29740163e44281fe679cf35db8b915aafeeb
Veronika Popova <ronipopova@gmail.com>
2025-02-02 14:12:44 +0200

    Enhance preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,20 +1,16 @@
-import re
+import json
 import stanza
 
 filter_attributes = ["cap", "body", "stem", "flesh", "gills", "tubes", "pores", "underside", "ring", "spores", "habitat"]
 
-stanza.download('bg', processors='tokenize,lemma, pos', verbose=False)
-nlp = stanza.Pipeline('bg', processors='tokenize,lemma, pos', download_method=None, verbose=False)
-
-def preprocess_mushrooms(mushrooms):    
-    return [preprocess_mushroom(mushroom) for mushroom in mushrooms]
+stanza.download('bg', processors='tokenize,lemma,pos')
+nlp = stanza.Pipeline('bg', processors='tokenize,lemma,pos', download_method=None)
 
 def preprocess_mushroom(mushroom):
     return {attribute:preprocess_text(mushroom[attribute]) for attribute in mushroom if attribute in filter_attributes}
 
 def preprocess_text(text):
     doc = nlp(text)
-
     lemmas = []
     for sentence in doc.sentences:
         for word in sentence.words:
@@ -22,3 +18,10 @@ def preprocess_text(text):
                 lemmas.append(word.lemma)
 
     return " ".join(lemmas)
+
+with open("corpus/parsed_mushrooms_data.json", "r", encoding="utf-8") as file:
+    mushrooms = json.load(file)
+    preprocessed_mushrooms = [preprocess_mushroom(mushroom) for mushroom in mushrooms]
+
+with open("corpus/preprocessed_mushrooms.json", "w", encoding="utf-8") as file:
+    json.dump(preprocessed_mushrooms, file, ensure_ascii=False, indent=4)
